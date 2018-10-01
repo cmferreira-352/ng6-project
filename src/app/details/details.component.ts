1fcb9c595f1b94ab3390b63d1b37ef185793f202
Cristhian Ferreira <cmferreira.352@gmail.com>
2018-10-01 14:14:16 -0300

    Adiciona projeto

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  user$: Object;
+
+  constructor(private route: ActivatedRoute, private data: DataService) {
+    this.route.params.subscribe(
+      param => this.user$ = param.id
+    )
+  }
 
   ngOnInit() {
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data
+    )
   }
 
 }
